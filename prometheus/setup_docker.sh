e91da24f6ba2bbd7c0f5c0688822908a057424a6
augusto.vaz <augusto.vaz@luizalabs.com>
2025-02-06 04:32:36 -0300

    feat: add docker setup for prometheus

--- prometheus/setup_docker.sh
+++ prometheus/setup_docker.sh
@@ -1,9 +1,6 @@
-TOKEN=$1
-ADDR=$2
 sudo apt update && sudo apt upgrade -y
 curl -fsSL get.docker.com | sudo sh
 sudo groupadd docker
 sudo usermod -aG docker $USER
-echo "All set, now run"
+echo "All set"
 newgrp docker
-echo "docker swarm join --token $TOKEN $ADDR"
