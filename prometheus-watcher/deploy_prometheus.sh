9b8308e772122794ac4f3147cabbf4adc6751a14
augustodsgv <augustodsgv@gmail.com>
2025-03-18 20:20:29 -0300

    chore: cleanup project

--- prometheus-watcher/deploy_prometheus.sh
+++
@@ -1,6 +0,0 @@
-docker run -it \
-    --name=prometheus_watcher \
-    -d \
-    -p 9090:9090 \
-    -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
-    quay.io/prometheus/prometheus 
--- prometheus/setup_docker.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-curl -fsSL get.docker.com | sudo sh
-sudo groupadd docker
-sudo usermod -aG docker $USER
-echo "All set"
-newgrp docker
